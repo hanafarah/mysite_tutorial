212bf6a76e8bd14ae975bcf79db2afaae8a2b294
Hana Farah'git config --global user.name Hana <farahhan@sheridancollege.ca>
2022-10-10 21:23:52 -0400

    updates to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,16 @@ from . import models
 
 # Register the `Post` model
 admin.site.register(models.Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'created',
+        'updated',
+
+
+    )
+
+
+admin.site.register(models.Post, PostAdmin)
